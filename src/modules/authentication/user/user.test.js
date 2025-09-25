b8e8d3e3b9927f7607e3c8eb98df4bbdcb716c05
Pol Quimerc'h <pol.quimerch@icloud.com>
2025-09-25 12:13:23 +0200

    Commit

--- src/modules/authentication/user/user.test.js
+++ src/modules/authentication/user/user.test.js
@@ -1,5 +1,55 @@
-import { describe, it } from "vitest";
+import { describe, it, vi, expect, afterEach } from "vitest";
+import { createUser } from './user.service.js';
+import { createUserInRepository } from './user.repository.js';
+
+
+vi.mock("./user.repository", async (importOriginal) => ({
+  ...(await importOriginal()),
+  createUserInRepository: vi.fn((data) => {
+    return {
+      id: 4,
+      name: data.name,
+      birthday: data.birthday,
+    };
+  }),
+}));
 
 describe("User Service", () => {
-  it("should create an user", async () => {});
+  afterEach(() => vi.clearAllMocks());
+
+  it("should create a user if old enough", async () => {
+    const user = await createUser({
+      name: "Valentin R",
+      birthday: new Date(1997, 8, 13),
+    });
+
+    expect(user).toBeDefined();
+    expect(user.id).toBe(4);
+    expect(user.id).toBeTypeOf("number");
+    expect(user).toHaveProperty("name", "Valentin R");
+    expect(user.birthday.getFullYear()).toBe(1997);
+    expect(user.birthday.getMonth()).toBe(8);
+  });
+	
+  it("should trigger a bad request error when user creation", async () => {
+    try {
+      await createUser({
+        name: "Valentin R",
+      });
+      assert.fail("createUser should trigger an error.");
+    } catch (e) {
+      expect(e.name).toBe('HttpBadRequest');
+      expect(e.statusCode).toBe(400);
+    }
+  });
+
+   it("should throw HttpForbidden if user is too young", async () => {
+    await expect(
+      createUser({
+        name: "Valentin R",
+        birthday: new Date(2010, 8, 13), // 15 ans en 2025
+      })
+    ).rejects.toThrow(HttpForbidden);
+  });
 });
+
